73f6d1a1ddbb8d9a24a47319943184d1d019acbc
flparvez <flparvez23@gmail.com>
2024-12-13 12:08:57 +0600

    robots change

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '*',
+      allow: ["/"],
       disallow: ["/admin/*"],
     },
     sitemap: `${baseUrl}sitemap.xml`,
