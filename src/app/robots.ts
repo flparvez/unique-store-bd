31afeb967b1cc304b90bd692a657562a7a22fcec
flparvez <flparvez23@gmail.com>
2024-12-17 11:51:55 +0600

    sitemap update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
     },
     sitemap: [
       `${baseUrl}sitemap.xml`,
-      `${baseUrl}products/sitemap.xml`
+      `${baseUrl}policy/sitemap.xml`
     ],
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,44 +1,20 @@
-import type { MetadataRoute } from 'next'
- 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://uniquestorebd.vercel.app/',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },  
-      {
-      url: 'https://uniquestorebd.vercel.app/products/sitemap.xml',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 0.9,
-    },
-    {
-      url: 'https://uniquestorebd.vercel.app/policy/returns',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://uniquestorebd.vercel.app/policy/replacement-warranty',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://uniquestorebd.vercel.app/policy/after-sales-support',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.3,
-    },
-    {
-      url: 'https://uniquestorebd.vercel.app/products/tws',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.3,
-    },
 
 
-  ]
+export default async function sitemap() {
+  const response = await fetch(`https://uniquestorebd-api.vercel.app/api/products`).then((res) => res.json())
+const products = response;
+  const baseUrl ="https://uniquestorebd.vercel.app/"
+const allProducts = products?.map((product:any) =>{
+ return {
+  url:`${baseUrl}product/${product?.slug}`,
+  lastModified: product?.createdAt,
+ }
+} )
+
+  return [{
+      url: baseUrl,
+      lastModified: new Date(),
+  },
+  ...allProducts
+]
 }
