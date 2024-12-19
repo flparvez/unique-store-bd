6e6f5da998237bbc085ab37d8d159af6e0913648
flparvez <flparvez23@gmail.com>
2024-12-19 11:59:27 +0600

    fix seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,18 +23,16 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   metadataBase: new URL("https://uniquestorebd.vercel.app/"),
   title: "Unique Store Bd | Quality Is Here",
-  description:
-    " Unique Store Bd. Authentic পণ্য কিনুন এবং দ্রুত ডেলিভারি পান। Authentic Tws, Xiaomi পণ্য, ঘড়ি, Smartwatch, Microphones,Headphone,Earbuds, WiFi Smart Switches, Home Automation পণ্য, Headphones, YouTube পণ্য কিনুন।বাংলাদেশের অনলাইন শপিংয়ের জন্য Unique Store BD Authentic পণ্য এবং দ্রুত ডেলিভারির নিশ্চয়তা। এখনই কিনুন এবং উপভোগ করুন।.",
+  description: "Enjoy hassle-free online shopping from Unique Store Bd. Find genuine products,  TWS earbuds, unique gadgets, home appliances, and daily essentials",
   robots: {
     index: true,
     follow: true,
   },
   keywords:
-    "Unique Store Bd, Quality Is Here, Ecommerce Website, Unique Shop Bd,Next Js",
+    "Unique Store Bd, Online Shopping in Bangladesh, Online Shopping Store, Buy Online, Shop Online, Electric Accessories, TWS, Unique Gadgets, Home Appliances, Daily Needs.",
   openGraph: {
     title: "Unique Store Bd | Quality Is Here",
-    description:
-      " Unique Store Bd. Authentic পণ্য কিনুন এবং দ্রুত ডেলিভারি পান। Authentic Tws, Xiaomi পণ্য, ঘড়ি, Smartwatch, Microphones,Headphone,Earbuds, WiFi Smart Switches, Home Automation পণ্য, Headphones, YouTube পণ্য কিনুন।বাংলাদেশের অনলাইন শপিংয়ের জন্য Unique Store BD Authentic পণ্য এবং দ্রুত ডেলিভারির নিশ্চয়তা। এখনই কিনুন এবং উপভোগ করুন।.",
+    description: "Enjoy hassle-free online shopping from Unique Store Bd. Find genuine products,  TWS earbuds, unique gadgets, home appliances, and daily essentials",
     images: ["https://i.ibb.co.com/WBVg5rv/image.png"],
     url: "https://uniquestorebd.vercel.app/",
   },
@@ -45,6 +43,15 @@ export const metadata: Metadata = {
     },
   },
 };
+
+
+
+
+
+
+
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
