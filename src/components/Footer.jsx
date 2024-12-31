d75cf447441651c6d564914fd9c23a5d31409651
flparvez <flparvez23@gmail.com>
2024-12-31 15:01:49 +0600

    footer edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,27 +6,7 @@ const Footer = () => {
 
 
 <footer className="bg-gray-900 text-white p-6 md:p-10">
-      <div className="max-w-7xl mx-auto">
-    
-        <p className="mb-4">
-          <span className="font-semibold text-yellow-500">Unique Store BD</span> - The Most Reliable eCommerce Site in Bangladesh. We maintain a comprehensive stock to ensure you receive your orders as quickly as possible. Experience online shopping in Bangladesh for authentic products, including the latest tech gadgets such as live streaming gear, YouTube studio setups, vlogging gear, home studio equipment, webcams, microphones, lighting setups, ring lights, smartphones, gimbals, and related products.
-        </p>
-        <p className="mb-4">
-          Enjoy the convenience of home delivery or courier service, or pick up your orders from our pickup point. We guarantee product quality, fast delivery, and exceptional after-sales support. Benefit from our free pre-sales and post-sales consultations to find the best budget-friendly gear.
-        </p>
-        <p className="mb-4">
-          Our technical team is dedicated to providing a seamless online shopping experience in Bangladesh, offering world-class, original products at reasonable prices. Our customer care team is always ready to assist you with your purchases and provide technical support.
-        </p>
-        <p className="mb-4">
-          With over one year of experience, we have earned the trust of millions of customers through authentic products and quality after-sales support, making us the most reliable and trusted platform in the Bangladesh eCommerce industry.
-        </p>
-        <p className="mb-4">
-          Join our vibrant online community to share your shopping experiences, learn about new products, and get answers to your questions. We are highly active on social media, especially in our Facebook Page:<Link href="https://www.facebook.com/uniquestorebd23" passHref><span className="font-semibold text-yellow-500">Unique Store BD</span></Link>, where we engage with customers to improve our services.
-        </p>
-        <p className="mb-4">
-          Whether you need live streaming equipment, YouTube studio gear, or other home studio equipment, <span className="font-semibold text-yellow-500">Unique Store BD</span> is your go-to recommendation. Help your friends by recommending us, and we'll provide friendly and professional assistance.
-        </p>
-      </div>
+
    
       <div className="container mx-auto text-center px-4">
         <h2 className="text-xl sm:text-2xl font-bold mb-2">Unique Store Bd | Quality Is Here</h2>
