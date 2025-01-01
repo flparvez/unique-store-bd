572cdd17f77d31b94f49858c7e18c7f5d6987953
flparvez <flparvez23@gmail.com>
2025-01-01 20:44:23 +0600

    sitemap add

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,20 +1,30 @@
-
-
 export default async function sitemap() {
-    const response = await fetch(`https://uniquestorebd-api.vercel.app/api/products`).then((res) => res.json())
+  const response = await fetch(`https://uniquestorebd-api.vercel.app/api/products`).then((res) => res.json());
   const products = response;
-    const baseUrl ="https://uniquestorebd.vercel.app/"
-  const allProducts = products?.map((product:any) =>{
-   return {
-    url:`${baseUrl}product/${product?.slug}`,
-    lastModified: product?.updatedAt,
-   }
-  } )
-  
-    return [{
-        url: baseUrl,
-        lastModified: new Date(),
-    },
+  const baseUrl = "https://uniquestorebd.vercel.app/";
+
+  // Array of static URLs
+  const staticUrls = [
+    { url: `${baseUrl}`, lastModified: new Date() },
+    { url: `${baseUrl}policy/returns`, lastModified: new Date() },
+    { url: `${baseUrl}policy/replacement-warranty`, lastModified: new Date() },
+    { url: `${baseUrl}products/table-lamp`, lastModified: new Date() },
+    { url: `${baseUrl}products/TWS`, lastModified: new Date() },
+    { url: `${baseUrl}products/Smart-Watch`, lastModified: new Date() },
+
+  ];
+
+  // Map product data to URLs
+  const allProducts = products?.map((product:any) => {
+    return {
+      url: `${baseUrl}product/${product?.slug}`,
+      lastModified: product?.updatedAt,
+    };
+  });
+
+  // Combine static URLs and product URLs
+  return [
+    ...staticUrls,
     ...allProducts
-  ]
-  }
+  ];
+}
