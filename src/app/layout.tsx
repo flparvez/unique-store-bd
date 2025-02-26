e56d225369fec757a351805c82745c75e86faad4
flparvez <flparvez23@gmail.com>
2025-02-26 18:51:42 +0600

    f size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
   },
   description: "Unique Store BD. Online Shopping BD. Enjoy hassle-free online shopping from Unique Store BD. Find genuine products,  TWS earbuds, unique gadgets, home appliances, and daily essentials",
   keywords:
-  "uniquestorebd,unique store , UNIQUE STORE Bd,  Online Shopping in Bangladesh, Online Shopping Store, Buy Online, Shop Online, Electric Accessories, TWS, Unique Gadgets, Home Appliances, Daily Needs.",
+  "uniquestorebd,unique store , UNIQUE STORE BD,  Online Shopping in Bangladesh, Online Shopping Store, Buy Online, Shop Online, Electric Accessories, TWS, Unique Gadgets, Home Appliances, Daily Needs.",
   robots: {
     index: true,
     follow: true,
