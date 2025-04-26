133cdf09bd7d13eb124e270c8bff7f01dcf21e3a
Md Parvez <flparvez23@gmail.com>
2025-04-26 16:39:39 +0600

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { GoogleTagManager } from "@next/third-parties/google";
 
-// Font optimization with font-display swap
+// Font optimization with font-display swa
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
